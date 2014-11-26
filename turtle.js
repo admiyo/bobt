00c5fc0932633fbe8aad8fb15cd07638b9cca0b6
Adam Young <ayoung@redhat.com>
2014-11-26 11:55:24 -0500

    animated turns

--- turtle.js
+++ turtle.js
@@ -1,13 +1,12 @@
 //Turtle.js
 
 function turtle_game(){
-  //state right = 0, down = 1, left = 2, up = 3
+  //state right = 0, down = 12, left = 8, up = 4
 
   var images = Array("turtle-right.png", "turtle-down.png",
                      "turtle-left.png", "turtle-up.png")
-
+  facing = 0;
   instruction_counter = 0;
-  state = 0 ;
   home_left = 0;
   home_top = 100;
   position_left = home_left;  
@@ -16,62 +15,84 @@ function turtle_game(){
    $("div").animate({left: position_left, top:position_top });
 
 function  update_position(){
-         if (state == 0) {
+         if (facing == 0) {
              position_left += 100;
          }
-         if (state == 1) {
+         if (facing == 12) {
              position_top += 100;
          }
-         if (state == 2) {
-             position_left -= 100;
-             
+         if (facing == 8) {
+             position_left -= 100;             
          }
-         if (state == 3) {
+         if (facing == 4) {
              position_top -= 100;
          } 
  }
 
- function move_forward(){
-     update_position();
-     $("div#turtle").
-         animate({left: position_left, top:position_top } );
+function  updated_top(){
+    if (facing == 12) {
+        return "+100";
+    }
+    if (facing == 4) {
+        return "-100";
+    } 
+    return position_top;
  }
 
-function clockwise(){
-        state++;
-        state = state % 4;
-        set_image();
-    }
 
+function  updated_left(){
+    if (facing == 0) {
+        return "+100";
+    }
+    if (facing == 8) {
+        return "-100";             
+    }
+    return position_left; 
+ }
+
+
+ function move_forward(){
+     $("div#turtle").animate({left: updated_left(), top:updated_top() },
+                             {start: update_position});
+ }
+
+ function turn_step(currentStep){
+     turn_to  = (16 + start - Math.round(currentStep)) % 16;
+     facing = turn_to;
+     image_name = "url(images/turtle-" + 
+         + turn_to +
+         "-16.png)";
+     $("div#turtle").css("background-image",image_name); 
+ }
 
  function turn_clockwise(){    
-     $("div#turtle").animate({},{start: clockwise});
+     $("div#turtle").animate(
+         {
+             textIndent: 4
+         },
+         {
+             duration: 3000,
+             step: turn_step,
+             start: function(){
+                 $("div#turtle").css( "text-indent", 0 );
+                 start = facing;
+             }
+         });
  }
 
-    function widdershins(){
-        state--;
-        if (state < 0 ) {
-            state = 3;
-        }
-        set_image();
-    }
-
-
 function turn_widdershins(){
-    $("div#turtle").animate({},{start: widdershins});
-}
-
-
-function set_image(){
-    for (i =0; i< 4;i++){
-       selector =  "#img" + i
-       span = $(selector);
-       if (state == i) {
-           $(span).css("display","inline")
-       }else{
-           $(span).css("display","none")
-       }
-   }
+     $("div#turtle").animate(
+         {
+             textIndent: 0
+         },
+         {
+             duration: 3000,
+             step: turn_step,
+             start: function(){
+                 $("div#turtle").css( "text-indent", 4);
+                 start = facing + 4;
+             }
+         });
 }
 
 
@@ -94,12 +115,19 @@ function step_program(){
 }
 
 function reset_turtle(){
-        instruction_counter = 0;
-        state = 0;
-        set_image();
-        position_left = home_left;  
-        position_top = home_top;  
-        $("div").animate({left: position_left, top:position_top });
+    instruction_counter = 0;
+    facing  = 0;
+
+     image_name = "url(images/turtle-"  
+         + facing +
+         "-16.png)";
+     $("div#turtle").css("background-image",image_name); 
+
+
+    $("div#turtle").css("background-image",image_name); 
+    position_left = home_left;  
+    position_top = home_top;  
+    $("div").animate({left: position_left, top:position_top });
 }
 
 function run_program(){
@@ -115,11 +143,9 @@ function run_program(){
      run_program();
   });
 
-
   $("button#step").click(function(){
      step_program();
   });
-
 
   $("button#forward").click(function(){
 
@@ -132,18 +158,18 @@ function run_program(){
 
      turn_clockwise()
   });
-
  
  $("button#widdershins").click(function(){
     $('input#program').val($('input#program').val() + 'w');
      turn_widdershins();  
   });
 
-
-
   $("button#reset").click(function(){
     reset_turtle();
   });
 
+  $("button#spin").click(function(){
+    spin();
+  });
 
 }
